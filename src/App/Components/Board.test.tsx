989bb44583234c69681f6c6ef5b44ed7463423b2
Fred-si <fred-si@zaclys.net>
2022-01-03 23:29:14 +0100

    fixup! [wip] Add winner verification

--- src/App/Components/Board.test.tsx
+++ src/App/Components/Board.test.tsx
@@ -1,25 +1,19 @@
-import Board, { Player } from "./Board"
+import Board, {
+    Player,
+    MoveAlreadyPlayedError,
+    WrongPlayerTurnError,
+    FullBoardError
+} from "./Board"
 
-let board: Board
-beforeEach(() => {
-    board = new Board()
-})
+const playMoves = (board: Board, cells: Array<number>, first: Player) => {
+    let second = first === Player.Cross ? Player.Circle : Player.Cross
 
-describe("Test constructor", () => {
-    it("should have property named cells", () => {
-        expect(board).toHaveProperty("cells")
-    })
-
-    it("should create 9 cells", () => {
-        expect(board.cells).toHaveLength(9)
-    })
-
-    it("should initialize all cells to undifined", () => {
-        expect(board.cells.every(e => e === undefined)).toBeTruthy()
-    })
-})
-
+    for (let i = 0; i < cells.length; i++) {
+        board.move(cells[i], i % 2 ? second : first)
+    }
+}
 describe("Test move", () => {
+    let board: Board
     beforeEach(() => {
         board = new Board()
     })
@@ -28,5 +22,70 @@ describe("Test move", () => {
         expect(() => board.move(9, Player.Cross)).toThrowError(RangeError)
     })
 
-    it.todo("should insert player on first cell")
+    it("should throw if cell index is less than 0", () => {
+        expect(() => board.move(-1, Player.Cross)).toThrowError(RangeError)
+    })
+
+    it("should throw if try to move twice in the same cell", () => {
+        board.move(0, Player.Cross)
+        expect(() => board.move(0, Player.Circle)).toThrowError(
+            MoveAlreadyPlayedError
+        )
+    })
+
+    it("should throw error when same player play twice move", () => {
+        board.move(0, Player.Cross)
+        expect(() => board.move(1, Player.Cross)).toThrowError(
+            WrongPlayerTurnError
+        )
+    })
+
+    it("should return list of 8 empty cells after first move", () => {
+        board.move(0, Player.Cross)
+        expect(board.emptyCells).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+    })
+
+    it("should return list of 7 empty cells after first move", () => {
+        board.move(0, Player.Cross)
+        board.move(5, Player.Circle)
+
+        expect(board.emptyCells).toEqual([1, 2, 3, 4, 6, 7, 8])
+    })
+
+    it("should throw when board is full", () => {
+        playMoves(board, [0, 1, 2, 4, 3, 6, 5, 8, 7], Player.Cross)
+
+        expect(() => board.move(0, Player.Circle)).toThrowError(FullBoardError)
+    })
+})
+
+describe("Test winner", () => {
+    let board: Board
+    beforeEach(() => {
+        board = new Board()
+    })
+
+    it("should return null when board is empty", () => {
+        expect(board.winner).toBe(null)
+    })
+
+    it("should return Player.Cross for win in lines", () => {
+        const winner = Player.Cross
+        playMoves(board, [0, 3, 1, 4, 2], winner)
+
+        expect(board.winner).toBe(winner)
+    })
+
+    it("should return Player.Circle for win in lines", () => {
+        const winner = Player.Circle
+        playMoves(board, [0, 3, 1, 4, 2], winner)
+
+        expect(board.winner).toBe(winner)
+    })
+
+    it.skip("should return false with full board and no winner", () => {
+        playMoves(board, [0, 1, 2, 4, 3, 6, 5, 8, 7], Player.Cross)
+
+        expect(board.winner).toBe(null)
+    })
 })
--- src/App/Components/Board.tsx
+++ src/App/Components/Board.tsx
@@ -1,17 +1,71 @@
 export enum Player {
-    Cross,
-    Circle
+    Cross = "cross",
+    Circle = "circle"
 }
 
+export class MoveAlreadyPlayedError extends Error {}
+export class WrongPlayerTurnError extends Error {}
+export class FullBoardError extends Error {}
+
 type Cell = Player | undefined
-
 export default class Board {
-    cells: Array<Cell>
+    private lastPlayer?: Player
+    private cells: Array<Cell> = [...Array(9)]
 
-    constructor() {
-        this.cells = Array(9)
-    }
     move(cellIndex: number, player: Player): void {
-        throw new RangeError(`Given index ${cellIndex} is out of range`)
+        if (cellIndex > 8 || cellIndex < 0) {
+            throw new RangeError(`Given index ${cellIndex} is out of range`)
+        }
+
+        if (this.lastPlayer === player) {
+            throw new WrongPlayerTurnError()
+        }
+
+        if (this.isFullBoard) {
+            throw new FullBoardError()
+        }
+
+        if (!this.isEmpty(cellIndex)) {
+            throw new MoveAlreadyPlayedError(
+                `Cell ${cellIndex} have already played`
+            )
+        }
+
+        this.lastPlayer = player
+        this.cells[cellIndex] = player
+    }
+
+    public isEmpty(cellIndex: number): boolean {
+        return !this.cells[cellIndex]
+    }
+
+    private get isFullBoard() {
+        return this.emptyCells.length === 0
+    }
+
+    get emptyCells(): Array<number> {
+        return this.cells.reduce<Array<number>>((acc, _, index) => {
+            if (this.isEmpty(index)) {
+                acc.push(index)
+            }
+
+            return acc
+        }, [])
+    }
+
+    get winner(): Player | null {
+        if (this.emptyCells.length === this.cells.length) {
+            return null
+        }
+
+        return Player.Cross
+    }
+
+    private get lines(): Array<Array<Cell>> {
+        return [
+            this.cells.slice(0, 3),
+            this.cells.slice(3, 6),
+            this.cells.slice(6)
+        ]
     }
 }
